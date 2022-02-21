57a2deb466c7a90f11c76b72297d5e0103747c5c
ankushchauhan9 <ankushchauhanbtech@gmail.com>
2022-02-21 15:33:17 +0530

    pipeline file commit

---
+++ pipeline.groovy
@@ -0,0 +1,30 @@
+pipeline{
+
+    agent any
+
+    triggers {
+        pollSCM('* * * * *')
+    }
+
+    stages{
+
+        stage('GitHub'){
+            steps{
+                git url:'https://github.com/ankushchauhan9/DockerTest.git'
+            }
+        }
+        stage('Build Gradle'){
+            steps{
+                bat 'gradlew clean build'
+            }
+        }
+
+    }
+
+    post{
+        always{
+            archiveArtifacts artifacts: '/build/libs/TestingDocker-0.0.1-SNAPSHOT.jar'
+        }
+    }
+
+}
