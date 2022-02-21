f4742d1df3b89256f38816772f125141ec09ad19
ankushchauhan9 <ankushchauhanbtech@gmail.com>
2022-02-21 14:57:26 +0530

    first commit

---
+++ src/main/java/com/example/TestingDocker/TestingDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestingDocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestingDocker/TestingDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestingDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
