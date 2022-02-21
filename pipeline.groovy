6908231f70ab9fb82a077905a5991c7b519f5df2
ankushchauhan9 <ankushchauhanbtech@gmail.com>
2022-02-21 17:52:49 +0530

    pipeline changes 9

--- pipeline.groovy
+++ pipeline.groovy
@@ -31,7 +31,7 @@ pipeline{
 
         stage('Login to Docker Hub') {
             steps{
-                bat 'docker login --username=$DOCKERHUB_CREDENTIALS_USR --password=$DOCKERHUB_CREDENTIALS_PSW'
+                bat 'echo $DOCKERHUB_CREDENTIALS_PSW | docker login -u $DOCKERHUB_CREDENTIALS_USR --password-stdin'
             }
         }
 
